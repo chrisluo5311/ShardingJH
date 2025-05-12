ab5e5ae99b48acebd5f2c4d4da6b045dff43b29d
chrisluo5311 <chrislo5311@gmail.com>
2025-05-12 10:59:55 -0700

    project init - add test url and sqlite configuration

---
+++ src/main/java/org/distributed/shardingjh/ShardingJhApplication.java
@@ -0,0 +1,15 @@
+package org.distributed.shardingjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class ShardingJhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/distributed/shardingjh/ShardingJhApplicationTests.java
@@ -0,0 +1,13 @@
+package org.distributed.shardingjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingJhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
