4f125f9359a8e8d6841eea4072078254ac46c1d7
chrisluo5311 <chrislo5311@gmail.com>
2025-05-15 01:15:01 -0700

    add constant class
    1. Redis Constant
    2. Shard Constant

---
+++ src/main/java/org/distributed/shardingjh/common/constant/RedisConst.java
@@ -0,0 +1,14 @@
+package org.distributed.shardingjh.common.constant;
+
+/**
+ * RedisConst
+ *
+ * @author Chris
+ */
+public class RedisConst {
+
+    /**
+     * Redis key prefix for user information
+     */
+    public static final String REDIS_KEY_PREFIX = "USER_";
+}
---
+++ src/main/java/org/distributed/shardingjh/common/constant/ShardConst.java
@@ -0,0 +1,18 @@
+package org.distributed.shardingjh.common.constant;
+
+/**
+ * ShardConst
+ *
+ * @author Chris
+ */
+public class ShardConst {
+
+    // Shard for regular users
+    public static final Integer TOTAL_SHARD_COMMON_COUNT = 2;
+    //Common shard prefix
+    public static final String SHARD_COMMON_PREFIX = "NORMAL_";
+    // Shard for orders
+    public static final String SHARD_ORDER_PREFIX = "ORDER_";
+    // Shard for old orders
+    public static final String SHARD_ORDER_OLD = "old";
+}
