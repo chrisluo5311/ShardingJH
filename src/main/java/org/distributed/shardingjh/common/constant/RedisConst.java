4cbac578e194947c17e1044ab787c5bdfb424b93
chrisluo5311 <chrislo5311@gmail.com>
2025-05-18 00:09:09 -0700

    update Redis config to accept JavaTime, Redis Key for user/order,

--- src/main/java/org/distributed/shardingjh/common/constant/RedisConst.java
+++ src/main/java/org/distributed/shardingjh/common/constant/RedisConst.java
@@ -10,5 +10,7 @@ public class RedisConst {
     /**
      * Redis key prefix for user information
      */
-    public static final String REDIS_KEY_PREFIX = "USER_";
+    public static final String REDIS_KEY_MEMBER_PREFIX = "USER_";
+
+    public static final String REDIS_KEY_ORDER_PREFIX = "ORDER_";
 }
--- src/main/java/org/distributed/shardingjh/config/RedisConfig.java
+++ src/main/java/org/distributed/shardingjh/config/RedisConfig.java
@@ -1,10 +1,13 @@
 package org.distributed.shardingjh.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -12,13 +15,25 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        return redisTemplate;
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        // Use constructor to supply ObjectMapper (not the deprecated setter)
+        Jackson2JsonRedisSerializer<Object> serializer =
+                new Jackson2JsonRedisSerializer<>(mapper, Object.class);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        // Optionally configure hash serializers as well if you use hashes
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        return template;
     }
 
 }
